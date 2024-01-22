df3a8fb04f7e143e7b42d0cd3250a5ed42c8a451
Usman Mehmood <bajwa345@gmail.com>
2024-01-22 17:30:42 +0500

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl : 'https://api.voogleapp.com/api',    
+    storageUrl : 'https://svoogle.blob.core.windows.net/data/', //'https://data.voogleapp.com/data/',
+    assetsUrl : 'https://svoogle.blob.core.windows.net/assets/' //'https://data.voogleapp.com/assets/'
+}; 
