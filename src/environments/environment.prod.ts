afb15243175f8e68d3ca88445b178ae5d56e0fc9
Usman Mehmood <bajwa345@gmail.com>
2024-03-23 21:40:42 +0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl : 'https://api.voogleapp.com/api',    
-    storageUrl : 'https://svoogle.blob.core.windows.net/data/', //'https://data.voogleapp.com/data/',
-    assetsUrl : 'https://svoogle.blob.core.windows.net/assets/' //'https://data.voogleapp.com/assets/'
+    storageUrl : 'https://svoogle.blr1.digitaloceanspaces.com/data/', //'https://data.pmln.app/data/',
+    assetsUrl : 'https://svoogle.blr1.digitaloceanspaces.com/assets/', //'https://data.pmln.app/assets/'
 }; 
